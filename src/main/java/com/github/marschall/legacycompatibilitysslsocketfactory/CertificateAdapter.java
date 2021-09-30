ee676cf00acfb757638816f3a17573f1dd29e328
Philippe Marschall <philippe.marschall@gmail.com>
2021-09-30 15:26:02 +0200

    bump

---
+++ src/main/java/com/github/marschall/legacycompatibilitysslsocketfactory/CertificateAdapter.java
@@ -0,0 +1,142 @@
+package com.github.marschall.legacycompatibilitysslsocketfactory;
+
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Principal;
+import java.security.PublicKey;
+import java.security.SignatureException;
+import java.util.Date;
+
+/**
+ * Adapts {@link java.security.cert.X509Certificate} to a {@link javax.security.cert.X509Certificate}.
+ */
+final class CertificateAdapter extends javax.security.cert.X509Certificate {
+
+  private final java.security.cert.X509Certificate delegate;
+
+  CertificateAdapter(java.security.cert.X509Certificate delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public void checkValidity() throws javax.security.cert.CertificateExpiredException, javax.security.cert.CertificateNotYetValidException {
+    try {
+      this.delegate.checkValidity();
+    } catch (java.security.cert.CertificateExpiredException e) {
+      javax.security.cert.CertificateExpiredException deprecatedException = new javax.security.cert.CertificateExpiredException(e.getMessage());
+      deprecatedException.initCause(e);
+      throw deprecatedException;
+    } catch (java.security.cert.CertificateNotYetValidException e) {
+      javax.security.cert.CertificateNotYetValidException deprecatedException = new javax.security.cert.CertificateNotYetValidException(e.getMessage());
+      deprecatedException.initCause(e);
+      throw deprecatedException;
+    }
+  }
+
+  @Override
+  public void checkValidity(Date date) throws javax.security.cert.CertificateExpiredException, javax.security.cert.CertificateNotYetValidException {
+    try {
+      this.delegate.checkValidity(date);
+    } catch (java.security.cert.CertificateExpiredException e) {
+      javax.security.cert.CertificateExpiredException deprecatedException = new javax.security.cert.CertificateExpiredException(e.getMessage());
+      deprecatedException.initCause(e);
+      throw deprecatedException;
+    } catch (java.security.cert.CertificateNotYetValidException e) {
+      javax.security.cert.CertificateNotYetValidException deprecatedException = new javax.security.cert.CertificateNotYetValidException(e.getMessage());
+      deprecatedException.initCause(e);
+      throw deprecatedException;
+    }
+  }
+
+  @Override
+  public int getVersion() {
+    return this.delegate.getVersion();
+  }
+
+  @Override
+  public BigInteger getSerialNumber() {
+    return this.delegate.getSerialNumber();
+  }
+
+  @Override
+  public Principal getIssuerDN() {
+    return this.delegate.getIssuerDN();
+  }
+
+  @Override
+  public Principal getSubjectDN() {
+    return this.delegate.getSubjectDN();
+  }
+
+  @Override
+  public Date getNotBefore() {
+    return this.delegate.getNotBefore();
+  }
+
+  @Override
+  public Date getNotAfter() {
+    return this.delegate.getNotAfter();
+  }
+
+  @Override
+  public String getSigAlgName() {
+    return this.delegate.getSigAlgName();
+  }
+
+  @Override
+  public String getSigAlgOID() {
+    return this.delegate.getSigAlgOID();
+  }
+
+  @Override
+  public byte[] getSigAlgParams() {
+    return this.delegate.getSigAlgParams();
+  }
+
+  @Override
+  public byte[] getEncoded() throws javax.security.cert.CertificateEncodingException {
+    try {
+      return this.delegate.getEncoded();
+    } catch (java.security.cert.CertificateEncodingException e) {
+      javax.security.cert.CertificateEncodingException deprecatedException = new javax.security.cert.CertificateEncodingException(e.getMessage());
+      deprecatedException.initCause(e);
+      throw deprecatedException;
+    }
+  }
+
+  @Override
+  public void verify(PublicKey key) throws javax.security.cert.CertificateException, NoSuchAlgorithmException, InvalidKeyException, NoSuchProviderException, SignatureException {
+    try {
+      this.delegate.verify(key);
+    } catch (java.security.cert.CertificateException e) {
+      javax.security.cert.CertificateException deprecatedException = new javax.security.cert.CertificateException(e.getMessage());
+      deprecatedException.initCause(e);
+      throw deprecatedException;
+    }
+  }
+
+  @Override
+  public void verify(PublicKey key, String sigProvider) throws javax.security.cert.CertificateException, NoSuchAlgorithmException,
+  InvalidKeyException, NoSuchProviderException, SignatureException {
+    try {
+      this.delegate.verify(key, sigProvider);
+    } catch (java.security.cert.CertificateException e) {
+      javax.security.cert.CertificateException deprecatedException = new javax.security.cert.CertificateException(e.getMessage());
+      deprecatedException.initCause(e);
+      throw deprecatedException;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return this.delegate.toString();
+  }
+
+  @Override
+  public PublicKey getPublicKey() {
+    return this.delegate.getPublicKey();
+  }
+
+}
---
+++ src/main/java/com/github/marschall/legacycompatibilitysslsocketfactory/LegacyCompatibilityExtendedSSLSession.java
@@ -0,0 +1,156 @@
+package com.github.marschall.legacycompatibilitysslsocketfactory;
+
+import java.security.Principal;
+import java.security.cert.Certificate;
+import java.util.List;
+
+import javax.net.ssl.ExtendedSSLSession;
+import javax.net.ssl.SNIServerName;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSessionContext;
+
+/**
+ * A {@link ExtendedSSLSession} that implements {@link #getPeerCertificateChain()}
+ * and delegates everything else to an actual {@link ExtendedSSLSession}.
+ */
+final class LegacyCompatibilityExtendedSSLSession extends ExtendedSSLSession {
+
+  private final ExtendedSSLSession delegate;
+
+  public LegacyCompatibilityExtendedSSLSession(ExtendedSSLSession delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public byte[] getId() {
+    return delegate.getId();
+  }
+
+  @Override
+  public SSLSessionContext getSessionContext() {
+    return delegate.getSessionContext();
+  }
+
+  @Override
+  public long getCreationTime() {
+    return delegate.getCreationTime();
+  }
+
+  @Override
+  public long getLastAccessedTime() {
+    return delegate.getLastAccessedTime();
+  }
+
+  @Override
+  public void invalidate() {
+    delegate.invalidate();
+  }
+
+  @Override
+  public boolean isValid() {
+    return delegate.isValid();
+  }
+
+  @Override
+  public void putValue(String name, Object value) {
+    delegate.putValue(name, value);
+  }
+
+  @Override
+  public Object getValue(String name) {
+    return delegate.getValue(name);
+  }
+
+  @Override
+  public void removeValue(String name) {
+    delegate.removeValue(name);
+  }
+
+  @Override
+  public String[] getValueNames() {
+    return delegate.getValueNames();
+  }
+
+  @Override
+  public Certificate[] getPeerCertificates() throws SSLPeerUnverifiedException {
+    return delegate.getPeerCertificates();
+  }
+
+  @Override
+  public Certificate[] getLocalCertificates() {
+    return delegate.getLocalCertificates();
+  }
+
+  @Override
+  public javax.security.cert.X509Certificate[] getPeerCertificateChain() throws SSLPeerUnverifiedException {
+    Certificate[] peerCertificates = this.getPeerCertificates();
+    javax.security.cert.X509Certificate[] certificateChain = new javax.security.cert.X509Certificate[peerCertificates.length];
+    for (int i = 0; i < peerCertificates.length; i++) {
+      java.security.cert.Certificate certificate = peerCertificates[i];
+      certificateChain[i] = new CertificateAdapter((java.security.cert.X509Certificate) certificate);
+
+    }
+    return certificateChain;
+  }
+
+  @Override
+  public Principal getPeerPrincipal() throws SSLPeerUnverifiedException {
+    return delegate.getPeerPrincipal();
+  }
+
+  @Override
+  public Principal getLocalPrincipal() {
+    return delegate.getLocalPrincipal();
+  }
+
+  @Override
+  public String getCipherSuite() {
+    return delegate.getCipherSuite();
+  }
+
+  @Override
+  public String getProtocol() {
+    return delegate.getProtocol();
+  }
+
+  @Override
+  public String getPeerHost() {
+    return delegate.getPeerHost();
+  }
+
+  @Override
+  public int getPeerPort() {
+    return delegate.getPeerPort();
+  }
+
+  @Override
+  public int getPacketBufferSize() {
+    return delegate.getPacketBufferSize();
+  }
+
+  @Override
+  public int getApplicationBufferSize() {
+    return delegate.getApplicationBufferSize();
+  }
+
+  @Override
+  public String[] getLocalSupportedSignatureAlgorithms() {
+    return delegate.getLocalSupportedSignatureAlgorithms();
+  }
+
+  @Override
+  public String[] getPeerSupportedSignatureAlgorithms() {
+    return delegate.getPeerSupportedSignatureAlgorithms();
+  }
+
+  @Override
+  public List<SNIServerName> getRequestedServerNames() {
+    return delegate.getRequestedServerNames();
+  }
+
+  @Override
+  public List<byte[]> getStatusResponses() {
+    return delegate.getStatusResponses();
+  }
+
+}
